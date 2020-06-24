d7cb556775ebc0d4b7ddae76660de2db1748ee18
blackgamer <blackgamer@mbj.nifty.com>
2020-06-24 16:02:42 +0900

    起動がうまくいかないのを修正

--- run.sh
+++ run.sh
@@ -1,21 +1,17 @@
 #! /bin/sh -x
+cd $(dirname $0)
 mkdir -p static/conf
 mkdir -p static/data
 mkdir -p static/html
-BASEFILES=`cd static/base; ls -1 *.md`
+BASEFILES=`cd static/base; ls -1`
 for f in $BASEFILES; do
-    if [ ! -f static/conf/$f ]; then
-       echo "copy $f to conf/"
-       cp static/base/$f static/conf/.
-    fi
-done
-BASEFILES2=`cd static/base; ls -1 *.png`
-for f in $BASEFILES; do
-    if [ ! -f static/html/$f ]; then
-       echo "copy $f to html/"
-       cp static/base/$f static/html/.
+    if [ ! -f static/data/$f ]; then
+       echo "copy $f to data/"
+       cp static/base/$f static/data/.
     fi
 done
 
 make
+
+PYTHONPATH=./static/conf; export PYTHONPATH
 python3 mdiary.py
