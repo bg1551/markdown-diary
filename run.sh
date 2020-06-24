4d5755ab69d3000f3c5b9ecf4f2959befacf1361
blackgamer <blackgamer@mbj.nifty.com>
2020-06-24 15:48:34 +0900

    再修正

--- run.sh
+++ run.sh
@@ -2,11 +2,18 @@
 mkdir -p static/conf
 mkdir -p static/data
 mkdir -p static/html
-BASEFILES=`cd static/base; ls -1`
+BASEFILES=`cd static/base; ls -1 *.md`
 for f in $BASEFILES; do
-    if [ ! -f static/data/$f ]; then
-       echo "copy $f"
-       cp static/base/$f static/data/.
+    if [ ! -f static/conf/$f ]; then
+       echo "copy $f to conf/"
+       cp static/base/$f static/conf/.
+    fi
+done
+BASEFILES2=`cd static/base; ls -1 *.png`
+for f in $BASEFILES; do
+    if [ ! -f static/html/$f ]; then
+       echo "copy $f to html/"
+       cp static/base/$f static/html/.
     fi
 done
 
